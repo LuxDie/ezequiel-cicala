afb0631639795b9434626cf3a2e243647135d427
Ezequiel Cicala <ezequielcicala@gmail.com>
2025-02-06 19:13:50 -0300

    add resume component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ResumeComponent } from "./resume/resume.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ResumeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
